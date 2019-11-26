fd587e7b08debf3399a31097dce865d85d825cc8
AndersonPaulaSilva <48701676+AndersonPaulaSilva@users.noreply.github.com>
2019-11-26 15:10:08 -0200

    Entity mapping

--- src/main/java/com/anderson/springapi/domain/RequestStage.java
+++ src/main/java/com/anderson/springapi/domain/RequestStage.java
@@ -6,17 +6,35 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+
+@Entity(name = "request_stage")
 public class RequestStage {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(columnDefinition = "text")
     private String description;
+
+    @Temporal(TemporalType.TIMESTAMP)
     private Date realizationDate;
+
+    @Column(length = 12, nullable = false)
+    @Enumerated(EnumType.STRING)
     private RequestState state;
+
+    @ManyToOne
+    @JoinColumn(name = "request_id", nullable = false)
     private Request request;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 }
--- src/main/java/com/anderson/springapi/domain/User.java
+++ src/main/java/com/anderson/springapi/domain/User.java
@@ -6,19 +6,37 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+
+@Entity(name = "user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(length = 20, nullable = false)
+    @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Column(length = 75, nullable = false)
     private String name;
+
+    @Column(length = 75, nullable = false, unique = true)
     private String email;
+
+    @Column(length = 100, nullable = false)
     private String password;
+
+    @OneToMany(mappedBy = "user")
     private List <Request> requests = new ArrayList<Request>();
+
+    @OneToMany(mappedBy = "user")
     private List <RequestStage> stages = new ArrayList<RequestStage>();
 }
